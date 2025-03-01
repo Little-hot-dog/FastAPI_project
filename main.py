679533c75614fb5b0f80e3af4f0957ca86598a37
nikita <zubastik_17@mail.ru>
2025-03-01 18:13:26 +0500

    второй коммит

--- main.py
+++ main.py
@@ -32,6 +32,40 @@ async def create_raw_data(raw_data: RawDataRequest, db: Session = Depends(get_db
 
     return raw_data
 
+@app.get("/get-system-info/")
+async def get_system_info(limit: int = 10, db: Session = Depends(get_db)):
+    data = db.query(SystemInfo).limit(limit).all()
+    return data
+
+
+@app.get("/get-system-info/{id}")
+async def get_system_info(id: Annotated[int, Path(..., title="Укажите id", ge=1)],
+                          db: Session = Depends(get_db)):
+    data = db.query(SystemInfo).filter(SystemInfo.id == id).first()
+    if SystemInfo is None:
+        return HTTPException(status_code=404, detail="Такой id не найден")
+    return data
+
+
+@app.delete("/delete-raw-and-system-info/{id}")
+async def delete_data(id: Annotated[int, Path(..., title="Укажите id", ge=1)],
+                      db: Session = Depends(get_db)):
+    sys_info = db.query(SystemInfo).filter(SystemInfo.id == id).first()
+    if sys_info:
+        db.delete(sys_info)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404, detail="Такой id не найден")
+
+    raw_data = db.query(SystemInfo).filter(RawData.id == id).first()
+    if raw_data:
+        db.delete(raw_data)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404, detail="Такой id не найден")
+
+
+
 def distribution_to_table(data: dict, db: Session):
     system_info = SystemInfo(
         host = data.get('host'),
@@ -40,6 +74,8 @@ def distribution_to_table(data: dict, db: Session):
     db.add(system_info)
     db.commit()
     db.refresh(system_info)
+
+
 
 # @app.post("/post-raw-data/")
 # async def create_raw_data(raw_data: RawDataRequest, db: Session = Depends(get_db)):
